578fdd8b399dcf2061a08ac2f333cc76060f703a
Brys | KHK <99002832+BrysWU@users.noreply.github.com>
2025-06-10 15:37:52 -0400

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -6,6 +6,7 @@ import {
   Button,
   CircularProgress,
   Link,
+  Stack,
 } from "@mui/material";
 import { useAuth } from "./AuthContext";
 
@@ -24,45 +25,69 @@ export default function Login({ onSwitch, onBack, onLoginSuccess }) {
   }
 
   return (
-    <Box sx={{ p: 3, minWidth: 320 }}>
-      <Typography variant="h4" sx={{ fontWeight: 900, mb: 2 }}>
+    <Box
+      sx={{
+        bgcolor: "#232f4b",
+        p: 4,
+        borderRadius: 4,
+        minWidth: 330,
+        boxShadow: 24,
+        mx: "auto",
+        my: 4,
+        color: "#fff",
+      }}
+    >
+      <Typography variant="h5" sx={{ fontWeight: 900, mb: 2, color: "#fff" }}>
         Login
       </Typography>
       <form onSubmit={handleSubmit}>
-        <TextField
-          label="Username"
-          fullWidth
-          autoFocus
-          sx={{ mb: 2 }}
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <TextField
-          label="Password"
-          fullWidth
-          type="password"
-          sx={{ mb: 2 }}
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        {err && (
-          <Typography color="error" sx={{ mb: 2 }}>
-            {err}
-          </Typography>
-        )}
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="secondary"
-          disabled={loading}
-        >
-          {loading ? <CircularProgress size={24} /> : "Login"}
-        </Button>
+        <Stack spacing={2}>
+          <TextField
+            label="Username"
+            fullWidth
+            autoFocus
+            sx={{
+              "& .MuiInputBase-root": { color: "#fff" },
+              "& .MuiInputLabel-root": { color: "#fff" },
+              input: { color: "#fff" },
+            }}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            InputLabelProps={{ style: { color: "#fff" } }}
+          />
+          <TextField
+            label="Password"
+            fullWidth
+            type="password"
+            sx={{
+              "& .MuiInputBase-root": { color: "#fff" },
+              "& .MuiInputLabel-root": { color: "#fff" },
+              input: { color: "#fff" },
+            }}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            InputLabelProps={{ style: { color: "#fff" } }}
+          />
+          {err && (
+            <Typography color="error" sx={{ mb: 1 }}>
+              {err}
+            </Typography>
+          )}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="secondary"
+            disabled={loading}
+            sx={{ fontWeight: 800, py: 1.2, borderRadius: 3, mt: 1 }}
+          >
+            {loading ? <CircularProgress size={24} /> : "Login"}
+          </Button>
+        </Stack>
       </form>
-      <Typography sx={{ mt: 2 }}>
+      <Typography sx={{ mt: 2, color: "#fff" }}>
         Don't have an account?{" "}
         <Link
           href="#"
@@ -70,11 +95,23 @@ export default function Login({ onSwitch, onBack, onLoginSuccess }) {
             e.preventDefault();
             onSwitch();
           }}
+          sx={{ color: "#ffb300", fontWeight: 700 }}
         >
           Sign up
         </Link>
       </Typography>
-      <Button sx={{ mt: 2 }} onClick={onBack} color="inherit" fullWidth>
+      <Button
+        sx={{
+          mt: 2,
+          fontWeight: 800,
+          borderRadius: 2,
+          bgcolor: "#3a4971",
+          color: "#fff",
+          "&:hover": { bgcolor: "#334166" },
+        }}
+        onClick={onBack}
+        fullWidth
+      >
         Back
       </Button>
     </Box>
